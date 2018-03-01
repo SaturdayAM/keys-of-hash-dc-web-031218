7257c4af83605e8d5e856e5c193412de825f0d87
Jerry Lee <jwl62@georgetown.edu>
2018-03-01 22:45:11 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,19 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
-  end
+    to_return = []
+    self.each do |key, value|
+      if arguments.inspect.include?(value.to_s)
+        to_return << key
+      end
+    end
+    to_return
+  end #end keys_of
+
 end
+
+hash = {
+  "key_1" => "val_1",
+  "key_2" => "val_2"
+}
+puts hash.keys_of("arg1", "val_2").inspect
